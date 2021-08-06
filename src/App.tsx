dafb0c5d6c3c58f476758c8460be872313be8d16
SwaminathanS1215 <swaminathan.shanmugam@wissen.com>
2021-08-06 15:22:47 +0530

    #10 Create Portfolio Summary Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
+	IonApp,
+	IonIcon,
+	IonLabel,
+	IonRouterOutlet,
+	IonTabBar,
+	IonTabButton,
+	IonTabs,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Login from './pages/login';
-import CreatePin  from './pages/CreatePortfolioPin';
+import CreatePin from './pages/CreatePortfolioPin';
+import PortfolioSummary from './pages/PortfolioSummary';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,21 +33,24 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-        <IonRouterOutlet>
-          <Route exact path="/login">
-            <Login />
-          </Route>     
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-          <Route exact path="/createpin">
-            <CreatePin />
-          </Route> 
-        </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp>
+	<IonApp>
+		<IonReactRouter>
+			<IonRouterOutlet>
+				<Route exact path="/login">
+					<Login />
+				</Route>
+				<Route exact path="/">
+					<Redirect to="/login" />
+				</Route>
+				<Route exact path="/createpin">
+					<CreatePin />
+				</Route>
+				<Route exact path="/summary">
+					<PortfolioSummary />
+				</Route>
+			</IonRouterOutlet>
+		</IonReactRouter>
+	</IonApp>
 );
 
 export default App;
