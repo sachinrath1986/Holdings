faf9179fc2013b5eb9607f2440afbe07728c769a
vivek-reddy-1234 <vivek.reddy@wissen.com>
2021-08-27 13:53:19 +0530

    made UI more accurate and cleaned up some code

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,9 +13,10 @@ import {
 } from '@ionic/react';
 import { useHistory } from 'react-router';
 import styles from './LoginPage.module.css';
-// import './LoginPage.css';
 
 const SIGN_IN_MSG = `Let's sign you in`;
+const TERMS_MSG = "By creating or logging into an account you agree to our Terms and Conditions and Privacy policies."
+
 const Login: React.FC = () => {
   const history = useHistory();
   const doLogin = () => {
@@ -27,65 +28,6 @@ const Login: React.FC = () => {
   return (
     <IonPage className={styles['login-page']}>
       <IonAvatar className={styles.logo} />
-
-      {/* const Login: React.FC = () => (
-  <IonPage  className={styles["login-page"]}>
-    <IonAvatar className={styles.logo} />
-
-    <div className={styles.header}>
-      <IonText color="light">
-        <h5>Welcome to Wissen</h5>
-      </IonText>
-      <IonText color="light">
-        <h2>{SIGN_IN_MSG}</h2>
-      </IonText>
-    </div>
-
-    <form className={styles["login-form"]}>
-      <IonLabel className={styles["input-label"]}>Email</IonLabel>
-      <IonInput
-        value=""
-        type="email"
-        placeholder="Enter email"
-        className={styles.input}
-      />
-      <IonLabel className={styles["input-label"]}>Password</IonLabel>
-      <IonInput
-        value=""
-        type="password"
-        placeholder="Enter password"
-        className={styles.input}
-      />
-
-      <IonGrid className={styles["terms-check-grid"]}>
-        <IonRow>
-          <IonCol size="1" className={styles["checkbox-col"]}>
-            <IonCheckbox 
-            className={styles["terms-check"]} />
-          </IonCol>
-          <IonCol size="11" className={styles["label-col"]}>
-            <IonLabel>
-              By creating or logging into an account you agree to our Terms and
-              Conditions and Privacy policies.
-            </IonLabel>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-
-      <IonButton className={styles["login-btn"]}>Login</IonButton>
-    </form>
-
-    <div className={styles["foot-note"]}>
-      <IonText>
-        <span>Not registered yet?&nbsp;</span>
-      </IonText>
-      <IonText className={styles["create-account"]}>
-        <span>Create an account.</span>
-      </IonText>
-    </div>
-
-  </IonPage>
-); */}
       <div className={styles.header}>
         <IonText color="light">
           <h5>Welcome to Wissen</h5>
@@ -118,8 +60,7 @@ const Login: React.FC = () => {
             </IonCol>
             <IonCol size="11" className={styles['label-col']}>
               <IonLabel>
-                By creating or logging into an account you agree to our Terms
-                and Conditions and Privacy policies.
+                {TERMS_MSG}
               </IonLabel>
             </IonCol>
           </IonRow>
